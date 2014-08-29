96a4b82b06f8b75768318fb2c6eef2f3a81b623f
Anthony Calandra <anthony@anthony-calandra.com>
2014-08-28 23:12:45 -0400

    Added comment on limitations with object literals macro.

--- examples/object-literals.js
+++ examples/object-literals.js
@@ -1,3 +1,11 @@
+/*
+  Some limitations due to sweet.js and readtables shown here:
+    - This macro isn't recursive, as you can see with __proto__.toString.
+    - Only works by redefining var which means it only works when defining
+      object literals with 'var'.
+    - The macro for 'super' is different than with the classes macro -- conflict.
+*/
+
 var x = 123;
 var ok = {
   __proto__: {
